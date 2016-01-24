54e9ba5fbeeff72324af156e306fb07db6ea744f
Ludwant <antonludwig01@yahoo.com>
2016-01-24 13:42:58 -0500

    State Machine
    Credit: David Austin

--- src/org/usfirst/frc/team5980/robot/Robot.java
+++ src/org/usfirst/frc/team5980/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 //test comment for GitHub - Anton L
+//test comment for GitHub - Stevie T
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -78,6 +79,42 @@ public class Robot extends IterativeRobot {
     	
     	
     	
+    }
+    
+    final static int FORWARD = 0;
+    final static int TURN = 1;
+    final static int FINISHED = 2;
+    final static int DONE = 3;
+    int state = FORWARD;
+    double speed = 0.5;
+    int forwardDistance = 30000;
+    int turnDistance = 1000;
+    public void autonomousInit() {
+    	rightMotorEncoder.reset();
+    }
+    public void autonomousPeriodicStateMachine() {
+    	switch(state) {
+    	case FORWARD:
+    		setDrivePower(speed, speed);
+    		if (rightMotorEncoder.get() < forwardDistance) {
+    			rightMotorEncoder.reset();
+    			state = TURN;
+    		}
+    		break;
+    	case TURN:
+    		setDrivePower(-speed, speed);
+    		if (rightMotorEncoder.get() < turnDistance) state = FINISHED;
+    		break;
+    	case FINISHED:
+    		setDrivePower(0,0);
+    		state = DONE;
+    		break;
+    	default:
+    	}
+    }
+    public void setDrivePower(double left, double right) {
+    	leftMotor.set(left);
+    	rightMotor.set(right);
     }
 
     /**
